108a795eb48ac72d3b4015a774e313d0861d6d57
Eska <emuhammedow1@gmail.com>
2026-05-30 17:28:36 +0500

    Added Task Transformer

---
+++ app/transformers/task_transformer.ts
@@ -0,0 +1,14 @@
+import { BaseTransformer } from '@adonisjs/core/transformers'
+import Task from '#models/task'
+
+export default class TaskTransformer extends BaseTransformer<Task> {
+  toObject() {
+    return this.pick(this.resource, [
+      'id',
+      'title',
+      'isCompleted',
+      'dueDate',
+      'createdAt'
+    ])
+  }
+}
